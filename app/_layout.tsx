58f16d915e8617a3bea029c761ae04ce17481622
Leo Bazan <leonardo.picinin@ciss.com.br>
2024-04-13 11:28:47 -0300

    adding react query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+
 import { Stack } from "expo-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/configs/reactQuery";
 
 export default function RootLayout() {
-  return <RootLayoutNav />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RootLayoutNav />
+    </QueryClientProvider>
+  );
 }
 
 const RootLayoutNav = () => {
